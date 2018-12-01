9a1b37000cceb46cf6d6e8d6771c614f76ea5b8e
sena_yuki <wanglei199909210927@gmail.com>
2018-12-02 00:12:02 +0800

    日常提交

--- build_mine.py
+++ build_mine.py
@@ -1,32 +1,32 @@
 import os
-import util
 import sqlite3
 import re
-img_mine_dict={}
-#处理content中的emoji
+img_file_dict={}
+img_info_dict={}
+cont_info_dict={}
 
-SQL="SELECT * FROM MINE LEFT JOIN IMAGE ON MINE.CID=IMAGE.CID GROUP BY MINE.CID ORDER BY MINE.TID,IMAGE.RANK"
 def build_ext_dict(dir,dict):#输入空字典，构建文件名扩展名对照 dict[文件名,.扩展名]
 	for file in os.listdir(os.getcwd()+"\\"+dir):
 		name=os.path.splitext(file)[0]
 		ext=os.path.splitext(file)[1]
 		dict[name]=ext
 		
-def getTag_Image(table,dir,dict,CID):#根据CID构建
+def getTag_Image(dir,img_file_dict,CID):#根据CID构建
 	#传入CID，数据库查IMAGE找对应图片，如果存在返回img标签，不存在则返回空字符串
-		img_tag=""
-		img_sql=c.execute("SELECT URL FROM "+table+" WHERE CID='"+str(CID)+"'")#查找这条私信是否附有图片
-		try:#遍历查询结果，如果为空，报异常退出
-			for img in img_sql:
-				url=img[0]
-				name=url[url.rfind("/")+1:]#处理链接
-				if name in imglist.keys():
-					name="../"+dir+"/"+name+dict[name]#得到完整文件名
-					img_tag=img_tag+'<div class="imgItem"> <img class="image" src="'+name+'"> </div>\n'
-			img_tag='<div class="picBox">\n'+img_tag+'</div>'
-		except:
-			None
-		return img_tag
+	img_tag=""
+	if CID in img_info_dict.keys():
+		dict=img_info_dict[CID]
+		for img in sorted(dict.keys()):#img是一个int类型的index
+			url=dict[img]
+			name=url[url.rfind("/")+1:]#处理链接
+			if name in img_file_dict.keys():
+				name="../"+dir+"/"+name+img_file_dict[name]#得到完整文件名
+				img_tag=img_tag+'<div class="imgItem"> <img class="image" src="'+name+'"> </div>\n'
+			img_tag='<div class="picBox">\n'+img_tag+'</div>\n'
+	else:
+		None
+	return img_tag
+
 def emojiInContent(content):#处理正文的emoji
 	pattern_emoji = re.compile(r'(<emoji=.*?>)', re.S)
 	emoji_urls=re.findall(pattern_emoji, content)
@@ -44,8 +44,18 @@ def imgInContent(dir,dict,content):#处理正文中的图片链接
 		content=content.replace(img,'<a class"imgInContent" href="../'+dir+'/'+file_name+'" target="_Blank"><i>图片</i></a>')
 	return content
 
-def build_content_list(content_table,img_table):
-	SQL="SELECT * FROM MINE LEFT JOIN IMAGE ON MINE.CID=IMAGE.CID GROUP BY MINE.CID ORDER BY MINE.TID,IMAGE.RANK"
+def build_img_dict(c,img_table,img_info_dict):
+	sql_list=c.execute("SELECT * FROM "+img_table)
+	for img in sql_list:
+		if img[0] not in img_info_dict.keys():#如果不存在这个
+			img_info_dict[img[0]]={}
+		img_info_dict[img[0]][img[1]]=img[2]
+
+def build_content_list(c,content_table,cont_info_dict):
+	sql_list=c.execute("SELECT * FROM "+content_table+" ORDER BY TID")
+	for content in sql_list:
+		cont_info_dict[content[0]]={"TID":content[1],"TYPE":content[2],"AUTHOR":content[3],"CONTENT":content[4],"TIME":content[5],"QAUTHOR":content[6],"QCONTENT":content[7],"QTIME":content[8],"MOOD":content[9],"LOCATION":content[10],"LONGITUDE":content[11],"LATITUDE":content[12]}
+
 def openSQLite(uname):
 	if (os.path.exists(uname+'.db')):
 		conn = sqlite3.connect(uname+'.db')
@@ -60,5 +70,6 @@ def openSQLite(uname):
 uname="wanglei19990921"
 print ("你输入的用户ID是: " + uname)
 c=openSQLite(uname)
-build_ext_dict("mine_images",img_mine_dict)
-print(imgInContent("MINE_images",img_mine_dict,content))
+build_ext_dict("mine_images",img_file_dict)
+build_img_dict(c,"IMAGE",img_info_dict)
+build_content_list(c,"MINE",cont_info_dict)
